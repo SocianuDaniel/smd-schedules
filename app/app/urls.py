2891e2d73ca66da3b832aab2d2e62ba568ad7a65
SocianuDaniel <socianudaniel@gmail.com>
2025-10-18 23:38:34 +0200

    init home app

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import urls as home_urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include(home_urls)),
 ]
